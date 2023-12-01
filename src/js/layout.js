03c684fe753eb6989515095578439dba62d1a060
MartaFagundez <55920614+MartaFagundez@users.noreply.github.com>
2023-12-01 17:20:14 -0300

    refactor(store): file rename

--- src/js/layout.js
+++ src/js/layout.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
-import { StarWarsProvider } from "./store/starwarsStore";
+import { StarWarsProvider } from "./store/starwars-store";
 
 import { Home } from "./views/home";
 
