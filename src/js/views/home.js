d406e8dfbef882cf744e1630897d22e11176b5a1
MartaFagundez <55920614+MartaFagundez@users.noreply.github.com>
2023-12-03 19:02:30 -0300

    feat(home): inital version done

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "../../styles/home.css";
 import Sidebar from "../component/sidebar";
+import poster from "../../img/starwars_poster.jpg";
 
 export default function Home() { 
 
@@ -8,9 +9,11 @@ export default function Home() {
 		<div className="d-flex w-100">
 			<Sidebar />
 
-			{/* Characters list */}
-			<main className="d-flex align-items-start w-100 p-4 mb-4">
-				<h1>Home</h1>
+			<main className="d-flex flex-column align-items-center justify-content-start w-100 p-4 mb-4">
+				<figure className="d-flex w-100" style={{maxWidth: "300px"}}>
+					<img src={poster} alt="Star Wars poster" className="w-100" />
+				</figure>
+				<h1 className="text-white-50 text-uppercase">Welcome!</h1>
 			</main>
 				
 		</div>
