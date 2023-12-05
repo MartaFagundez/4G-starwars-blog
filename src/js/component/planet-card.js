2cd4779d350a67fa3755ef12a4b2333016045c2a
MartaFagundez <55920614+MartaFagundez@users.noreply.github.com>
2023-12-04 22:23:52 -0300

    feat(favorites): add/remove planet

--- src/js/component/planet-card.js
+++ src/js/component/planet-card.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import defaultImg from "../../img/default-img.jpg";
 import { Link } from 'react-router-dom';
+import { useFavoritesContext } from '../contexts/favorites-context';
 
 export default function PlanetCard({planet}) {
+    const {store, actions } = useFavoritesContext();
+    
     const planetImageUrl = `https://starwars-visualguide.com/assets/img/planets/${planet.uid}.jpg`;
 
-    function handleFavorite() {
-        console.log("to-do: handleFavorite");
+    const isPlanetInFavorites = store.planets.some(
+        (favPlanet) => favPlanet.uid === planet.uid
+      );
+    
+    const handleToggleFavorite = () => {
+    if (isPlanetInFavorites) {
+        actions.removePlanetFavorite(planet.uid);
+    } else {
+        actions.addPlanetFavorite(planet);
     }
+    };
 
     return (
 
@@ -26,7 +37,7 @@ export default function PlanetCard({planet}) {
                 <div className="card-body">
                     <h6 className="card-title text-white text-uppercase fw-bold mb-4">{planet.name}</h6>
                     <div className="d-flex justify-content-between align-items-center">
-                        <p className="btn p-0 m-0" onClick={handleFavorite}><i className="fa-regular fa-heart text-warning fs-5"></i></p>
+                        <p className="btn p-0 m-0" onClick={handleToggleFavorite}><i className={`text-warning fs-5 fa-heart ${isPlanetInFavorites ? "fa-solid" : "fa-regular"}`}></i></p>
                         <Link to={`/planets/${planet.uid}`} className="btn btn-outline-warning">See More</Link>
                     </div>
                 </div>
