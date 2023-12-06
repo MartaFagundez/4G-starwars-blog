192b31c86f48d220a3fd27711780c420c969a1a3
MartaFagundez <55920614+MartaFagundez@users.noreply.github.com>
2023-12-06 16:35:56 -0300

    refactor(characters-list componenet): make it receive the characters by parameter

--- src/js/component/characters-list.js
+++ src/js/component/characters-list.js
@@ -1,17 +1,14 @@
 import React from 'react';
-import { useCharactersContext } from '../contexts/characters-context';
 import loadingImg from "../../img/loading.gif";
 import CharacterCard from './character-card';
 
-export default function CharactersList() {
-    const {store: charactersStore, actions} = useCharactersContext();
-    
+export default function CharactersList({characters}) {
 
     return (
             <main className="d-flex align-items-start w-100 p-4 mb-4">
              {/*LOADING  */}
             {
-                charactersStore.characters.length === 0 && 
+                characters.length === 0 && 
                     <div className="d-flex flex-column align-items-center w-100 p-4">
                         <figure className='d-flex mb-3' style={{width: "150px"}}>
                             <img src={loadingImg} alt="" className='w-100'/>
@@ -22,10 +19,10 @@ export default function CharactersList() {
 
             {/* LIST */}
             {
-                charactersStore.characters.length > 0 && 
+                characters.length > 0 && 
                     <ul className="d-flex flex-wrap justify-content-center align-items-start w-100">
                         { 
-                            charactersStore.characters.map((character) => {
+                            characters.map((character) => {
                                 return	(
                                     <li key={character.uid}>
                                         <CharacterCard character={character} />
--- src/js/views/characters.js
+++ src/js/views/characters.js
@@ -2,13 +2,15 @@ import React from "react";
 import "../../styles/home.css";
 import Sidebar from "../component/sidebar";
 import CharactersList from "../component/characters-list";
+import { useCharactersContext } from "../contexts/characters-context";
 
 export default function Characters() { 
+	const {store} = useCharactersContext();
 
 	return (
 		<div className="container-fluid d-flex w-100" style={{maxWidth: "1400px"}}>
 			<Sidebar />
-			<CharactersList />
+			<CharactersList characters={store.characters}/>
 				
 		</div>
 	)
