75a1162beef3dd7ca369852332d53f2b60507d8d
MartaFagundez <55920614+MartaFagundez@users.noreply.github.com>
2023-12-06 16:38:41 -0300

    refactor(planets-list componenet): make it receive the planets by parameter

--- src/js/component/planets-list.js
+++ src/js/component/planets-list.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import { usePlanetsContext } from '../contexts/planets-context';
 import loadingImg from "../../img/loading.gif";
 import PlanetCard from './planet-card';
 
-export default function PlanetsList() {
-    const {store: planetsStore, actions} = usePlanetsContext();
+export default function PlanetsList({planets}) {
     
 
     return (
             <main className="d-flex align-items-start w-100 p-4 mb-4">
              {/*LOADING  */}
             {
-                planetsStore.planets.length === 0 && 
+                planets.length === 0 && 
                     <div className="d-flex flex-column align-items-center w-100 p-4">
                         <figure className='d-flex mb-3' style={{width: "150px"}}>
                             <img src={loadingImg} alt="" className='w-100'/>
@@ -22,10 +20,10 @@ export default function PlanetsList() {
 
             {/* LIST */}
             {
-                planetsStore.planets.length > 0 && 
+                planets.length > 0 && 
                     <ul className="d-flex flex-wrap justify-content-center align-items-start w-100">
                         { 
-                            planetsStore.planets.map((planet) => {
+                            planets.map((planet) => {
                                 return	(
                                     <li key={planet.uid}>
                                         <PlanetCard planet={planet} />
--- src/js/views/planets.js
+++ src/js/views/planets.js
@@ -2,13 +2,15 @@ import React from "react";
 import "../../styles/home.css";
 import Sidebar from "../component/sidebar";
 import PlanetsList from "../component/planets-list";
+import { usePlanetsContext } from "../contexts/planets-context";
 
 export default function Planets() { 
+	const {store} = usePlanetsContext();
 
 	return (
 		<div className="container-fluid d-flex w-100" style={{maxWidth: "1400px"}}>
 			<Sidebar />
-			<PlanetsList />
+			<PlanetsList planets={store.planets}/>
 				
 		</div>
 	)
